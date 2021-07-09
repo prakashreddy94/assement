f0f342d2b37d153c46a816b4b3a013c2882777a8
prakashreddy94 <prakash94.reddy@gmail.com>
2021-07-09 10:40:37 +0530

    new pom

---
+++ src/main/java/sarath/App.java
@@ -0,0 +1,16 @@
+package sarath;
+
+/**
+ * Hello world!
+   this is sarath 
+iam from anantapur
+
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
